ecae08ef6c6ee5720c867f83f4a958e4324fae5c
1SurajDesai <surajdesai219@gmail.com>
2022-03-02 11:23:50 +0530

    Adding files for Rice data management

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace InventaroryDataManagmentDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "C:/Visual Studio/InventoryDataManagementDemo/Rice.jason";
+
+            FetchJasonForRice fetchJasonForRice = new FetchJasonForRice();
+            Rice data = fetchJasonForRice.Read(path);
+
+            for (int i = 0; i < data.typeofRice.Count; i++)
+            {
+                Console.WriteLine(data.typeofRice[i].name);
+                Console.WriteLine(data.typeofRice[i].weight);
+                Console.WriteLine(data.typeofRice[i].price);
+                Console.WriteLine();
+            }
+
+
+            //Console.WriteLine(data.weight);
+            //Console.WriteLine(data.price);
+        }
+
+    }
+}
+
---
+++ Rice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaroryDataManagmentDemo
+{
+    public class Rice
+    {
+        public List<TypeofRice> typeofRice;
+    }
+    public class TypeofRice
+    {
+        public string name;
+        public string weight;
+        public int price;
+
+    }
+}
